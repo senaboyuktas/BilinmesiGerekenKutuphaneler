2aa851dbd04339818adce1da54603ac0316af710
senaboyuktas <senabykts99@gmail.com>
2024-02-08 14:17:01 +0300

    Add project files.

---
+++ UdemyCoreLibrary/FluentValidationApp.Web/Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FluentValidationApp.Web.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        //[Required(ErrorMessage = "Name alanı boş olamaz. Attribute'dan geldi.")]
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public DateTime? BirthDay { get; set; }
+    }
+}
